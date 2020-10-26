c67d2f71daaa69540051b2820b7bf36e646e5785
massartval <massart.val@gmail.com>
2020-10-26 14:54:39 +0100

    README.md

--- assets/js/multiplier.js
+++ assets/js/multiplier.js
@@ -2,23 +2,40 @@ let scoreTotal;
 let scoreOnClick = 1;
 let multiplierPrice = 15;
 
-
- /**
-  * Fonction multiplicateur
-  */multiply() {
+/**
+ * Fonction multiplicateur
+ */multiply() {
     buyMultiplier();
+    increaseMultiplierPrice();
     increaseScoreOnClick();
-}
+    displayMultiplier();
+    tellMultiplierPrice();
+ }
 /**
  * Fonction acheter multiplicateur
  */buyMultiplier() {
-     scoreTotal = scoreTotal - multiplierPrice;
-     multiplierPrice = multiplierPrice * 1.15;
-     return Math.floor(multiplierPrice);
+    scoreTotal = scoreTotal - multiplierPrice;
+    return scoreTotal;
+ }
+/**
+ * Fonction augmenter prix multiplicateur
+ */increaseMultiplierPrice() {
+    multiplierPrice = multiplierPrice * 1.15;
+    return Math.floor(multiplierPrice);
  }
 /**
  * Fonction augmenter multiplicateur
  */increaseScoreOnClick() {
     let scoreIncreaseOnClick = scoreIncreaseOnClick + 1;
     return scoreIncreaseOnClick;
+ }
+/**
+ * Fonction afficher multiplicateur
+ */displayMultiplier() {
+    document.getElementById("multiplier").innerHTML = `Multiplier x${scoreIncreaseOnClick}`
+ }
+/**
+ * Fonction afficher coût amélioration multiplicateur
+ */tellMultiplierPrice() {
+    document.getElementById('runMultiplier').innerHTML = `Améliorer multiplicateur: ${multiplierPrice}`
  }
