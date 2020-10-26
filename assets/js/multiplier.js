30f4e82eaec5e2df48ef91b96faa5ab60bf8ee5b
massartval <massart.val@gmail.com>
2020-10-26 14:18:29 +0100

    Fonction Fonction afficher coût amélioration multiplicateur

--- assets/js/multiplier.js
+++ assets/js/multiplier.js
@@ -2,19 +2,25 @@ let scoreTotal;
 let scoreOnClick = 1;
 let multiplierPrice = 15;
 
-
- /**
-  * Fonction multiplicateur
-  */multiply() {
+/**
+ * Fonction multiplicateur
+ */multiply() {
     buyMultiplier();
+    increaseMultiplierPrice();
     increaseScoreOnClick();
-}
+    displayMultiplier();
+ }
 /**
  * Fonction acheter multiplicateur
  */buyMultiplier() {
-     scoreTotal = scoreTotal - multiplierPrice;
-     multiplierPrice = multiplierPrice * 1.15;
-     return Math.floor(multiplierPrice);
+    scoreTotal = scoreTotal - multiplierPrice;
+    return scoreTotal;
+ }
+/**
+ * Fonction augmenter prix multiplicateur
+ */increaseMultiplierPrice() {
+    multiplierPrice = multiplierPrice * 1.15;
+    return Math.floor(multiplierPrice);
  }
 /**
  * Fonction augmenter multiplicateur
@@ -22,3 +28,15 @@ let multiplierPrice = 15;
     let scoreIncreaseOnClick = scoreIncreaseOnClick + 1;
     return scoreIncreaseOnClick;
  }
+/**
+ * Fonction afficher multiplicateur
+ */displayMultiplier() {
+    document.getElementById("multiplier").innerHTML = `Multiplier x${scoreIncreaseOnClick}`
+ }
+/**
+ * Fonction afficher coût amélioration multiplicateur
+ */tellMultiplierPrice() {
+    document.getElementById('runMultiplier').innerHTML = `Améliorer multiplicateur: ${multiplierPrice}`
+ }
+ 
+
